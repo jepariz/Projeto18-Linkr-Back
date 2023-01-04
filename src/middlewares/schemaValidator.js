e64f63ce9d11e26bd02be56c805a37af41a83155
jepariz <je.parizotto@gmail.com>
2023-01-03 22:14:55 -0300

    fix: error messages on joi validation

--- src/middlewares/schemaValidator.js
+++ src/middlewares/schemaValidator.js
@@ -3,7 +3,7 @@ export function validateSchema(schema) {
       console.log(req.body)
       const {error} = schema.validate(req.body, {abortEarly: false});
       if (error) {
-        return res.status(422).send(error.details.map(detail => detail.message));
+        return res.status(422).send(error.message);
       }
   
       res.locals = req.body
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,10 +1,13 @@
 import joi from 'joi';
 
 const userSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().required(),
-  username: joi.string().required(),
-  photo: joi.string().uri().required()
+  email: joi.string()
+  .email()
+  .required()
+  .error(new Error('O campo email é obrigatório e deve ser um endereço válido')),
+  password: joi.string().required().error(new Error('Todos os campos são obrigatórios!')),
+  username: joi.string().required().error(new Error('Todos os campos são obrigatórios!')),
+  photo: joi.string().uri().required().error(new Error('Todos os campos são obrigatórios!'))
 });
 
 export default userSchema;
