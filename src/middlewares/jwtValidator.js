02e0b542a11ccc2948aa1d881e7bb548ae348010
Lucas F. Silva <lucasdotfs@gmail.com>
2023-01-05 15:03:58 -0300

    fix: jwt validation

--- src/middlewares/jwtValidator.js
+++ src/middlewares/jwtValidator.js
@@ -14,15 +14,17 @@ export default function (req, res, next) {
     process.env.SECRET,
     async (error, token) => {
       if (error) return res.sendStatus(401);
-
+      // NAO TEM EMAIL NO TOKEN
+      console.log(token);
       const queryString = `
-        SELECT * FROM USERS WHERE EMAIL = $1
+        SELECT * FROM USERS WHERE USERS.USERNAME = $1
       `;
 
       try {
-        const users = await connection.query(queryString, [token.email]);
+        console.log(token.username);
+        const users = await connection.query(queryString, [token.username]);
         if (users.rows.length === 0) return res.sendStatus(401);
-        res.locals.user = users[0];
+        res.locals.user = users.rows[0];
       } catch (error) {
         return res.sendStatus(401);
       }
