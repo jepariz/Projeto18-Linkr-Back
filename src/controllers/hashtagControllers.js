9ba051dcccfe3109ecf54ca7281e3bd383aae05e
Guilherme Morais <guilherme.morais0111@gmail.com>
2023-01-06 16:23:14 -0300

    feat: create route to each hashtag

--- src/controllers/hashtagControllers.js
+++ src/controllers/hashtagControllers.js
@@ -1,9 +1,28 @@
-import { getTrending } from "../repositories/hashtagRepository.js";
+import { getHashtagByName, getTrending } from "../repositories/hashtagRepository.js";
+import { getPostsByHashtagID } from "../repositories/postRepository.js";
 
 export async function trending(req, res) {
   try {
     const popularHashtags = await getTrending();
     return res.status(200).send(popularHashtags.rows);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send(err.message);
+  }
+}
+
+export async function postsByHashtag(req,res) {
+  try {
+    const results = await getHashtagByName(req.params.hashtag);
+    if(results.rows.length===0) {
+      return res.status(200).send([]);
+    }
+
+    const hashtagID = results.rows[0].id;
+    const { error, posts } = await getPostsByHashtagID(hashtagID,20);
+    if (error) return res.sendStatus(500);
+
+    return res.send(posts);
   } catch (err) {
     console.log(err.message);
     return res.status(500).send(err.message);
--- src/routes/hashtagRoutes.js
+++ src/routes/hashtagRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { trending } from "../controllers/hashtagControllers.js";
+import { postsByHashtag, trending } from "../controllers/hashtagControllers.js";
+import jwtValidator from "../middlewares/jwtValidator.js";
 
 const router = Router();
 
 router.get("/trending", trending);
+router.get("/hashtag/:hashtag", jwtValidator, postsByHashtag);
 
 export default router;
